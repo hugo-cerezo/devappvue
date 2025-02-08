0c61cad52f04757e71031aa4cd9e6e131c9ae1ae
Enzo Mandine <enzo.mandine@laplateforme.io>
2025-02-08 21:02:20 +0100

    update Calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -3,10 +3,13 @@ import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 import bootstrap5Plugin from '@fullcalendar/bootstrap5'
+import { ref } from 'vue'
 
 // import the third-party stylesheets directly from your JS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css' // needs additional webpack config!
+
+const events = ref([{ title: 'Bolognaise', date: '2025-02-10', allDay: true }])
 
 export default {
   components: {
@@ -17,34 +20,39 @@ export default {
       calendarOptions: {
         plugins: [dayGridPlugin, interactionPlugin, bootstrap5Plugin],
         initialView: 'dayGridMonth',
-        dateClick: (arg: { dateStr: string }) => this.handleDateClick(arg),
-        events: [{ title: 'event 1', date: '2025-02-10' }],
         weekends: false,
+        selectable: true,
+        editable: true,
+        dateClick: (arg: { dateStr: string }) => this.handleDateClick(arg),
+        eventClick: (arg: any) => this.eventClick(arg),
+        // select: (arg: { startStr: string; endStr: string }) => this.handleDateSelect(arg),
+        events: events.value,
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridYear,dayGridMonth,dayGridWeek,dayGridDay',
+        },
       },
     }
   },
   methods: {
-    handleDateClick: function (arg: { dateStr: string }) {
+    handleDateClick: (arg: { dateStr: string }) => {
       alert('date click! ' + arg.dateStr)
     },
-    changeView: function (view: string) {
-      console.log((this.$refs.fullCalendar as any).getApi().changeView(view))
+    eventClick: (arg: any) => {
+      if (confirm('Are you sure you want to delete this event?')) {
+        arg.event.remove()
+      }
     },
+    // handleDateSelect: (arg: { startStr: string; endStr: string }) => {
+    //   alert('selected ' + arg.startStr + ' to ' + arg.endStr)
+    // },
   },
 }
 </script>
 
 <template>
-  <div>
-    <div class="d-flex flex-row">
-      <button @click="changeView('dayGridYear')" class="viewBtn btn btn-primary">Year</button>
-      <button @click="changeView('dayGridMonth')" class="viewBtn btn btn-primary">Month</button>
-      <button @click="changeView('dayGridWeek')" class="viewBtn btn btn-primary">Week</button>
-      <button @click="changeView('dayGridDay')" class="viewBtn btn btn-primary">Day</button>
-      <button @click="changeView('dayGrid')" class="viewBtn btn btn-primary">Generic</button>
-    </div>
-    <FullCalendar id="calendar" ref="fullCalendar" :options="calendarOptions" />
-  </div>
+  <FullCalendar id="calendar" :options="calendarOptions" />
 </template>
 
 <style lang="css" scoped>
