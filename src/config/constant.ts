61e202ad95747fc895ea050bbcbf7ed0910777d4
Enzo Mandine <enzo.mandine@laplateforme.io>
2025-02-08 14:31:18 +0100

    config folder for constat storage, added default config for GoogleSingInPlugin

---
+++ src/config/constant.ts
@@ -0,0 +1,3 @@
+// Replace with your own client ID (this is a test client ID from Enzo's account)
+export const GOOGLE_CLIENT_ID =
+  '531767569977-p791pp677m3j5h2sto6m9fqb91lat0qi.apps.googleusercontent.com'
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import './assets/main.css'
 import './assets/menus.css'
+import GoogleSignInPlugin from 'vue3-google-signin'
+import { GOOGLE_CLIENT_ID } from './config/constant'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +10,8 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
+app.use(GoogleSignInPlugin, {
+  clientId: GOOGLE_CLIENT_ID,
+})
 
 app.mount('#app')
