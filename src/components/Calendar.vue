0e4a88d20de5bdef41e84f2da2aa2e68450117b5
Enzo Mandine <enzo.mandine@laplateforme.io>
2025-02-08 15:24:28 +0100

    added Calendar component and Agenda view + route

---
+++ src/components/Calendar.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: (arg: { dateStr: string; }) => this.handleDateClick(arg),
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg: { dateStr: string; }) {
+      alert('date click! ' + arg.dateStr)
+    }
+  }
+}
+</script>
+
+<template>
+    <FullCalendar :options="calendarOptions" />
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MenuView from '../views/MenuView.vue'
+import Agenda from '@/views/Agenda.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +17,11 @@ const router = createRouter({
       component: MenuView,
     },
     {
+      path: '/agenda',
+      name: 'agenda',
+      component: Agenda,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
@@ -23,7 +29,6 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
-    
   ],
 })
 export default router
